1c877a827bba72b7f90129b60232d4c8133d9c44
Samuel <142357575+prof-sd1@users.noreply.github.com>
2025-10-11 21:14:02 +0300

    Create task21.go

---
+++ task21.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// WordFrequency returns a map of word counts (case-insensitive, ignoring punctuation)
+func WordFrequency(text string) map[string]int {
+	wordCount := make(map[string]int)
+
+	// Convert to lowercase
+	text = strings.ToLower(text)
+
+	// Remove punctuation using regex
+	re := regexp.MustCompile(`[^\w\s]`)
+	cleanText := re.ReplaceAllString(text, "")
+
+	// Split into words
+	words := strings.Fields(cleanText)
+
+	// Count frequency
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	return wordCount
+}
